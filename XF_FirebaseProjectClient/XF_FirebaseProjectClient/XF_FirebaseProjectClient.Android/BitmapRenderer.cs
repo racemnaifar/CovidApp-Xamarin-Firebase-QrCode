658ec371ff4d4851ed33f62ea7498b9785a815c2
racem naifar <racem.naifar@iit.ens.tn>
2021-02-08 22:26:55 +0100

    Last Version

---
+++ XF_FirebaseProjectClient/XF_FirebaseProjectClient/XF_FirebaseProjectClient.Android/BitmapRenderer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+using XF_FirebaseProjectClient.Models;
+using XF_FirebaseProjectClient.Renderers;
+using ZXing;
+using ZXing.Common;
+using ZXing.Rendering;
+
+namespace XF_FirebaseProjectClient.Droid
+{
+    public class BitmapRenderer : IBarcodeRenderer<Bitmap>
+    {
+        private readonly IQRColor qRColor;
+
+        public BitmapRenderer(IQRColor qRColor)
+        {
+            this.qRColor = qRColor;
+        }
+
+        public Bitmap Render(BitMatrix matrix, BarcodeFormat format, string content)
+        {
+            return Render(matrix, format, content, new EncodingOptions());
+        }
+
+        public Bitmap Render(BitMatrix matrix, BarcodeFormat format, string content, EncodingOptions options)
+        {
+            var width = matrix.Width;
+            var height = matrix.Height;
+            var pixels = new int[width * height];
+            var outputIndex = 0;
+            for (var y = 0; y < height; y++)
+            {
+                for (var x = 0; x < width; x++)
+                {
+                    pixels[outputIndex] = matrix[x, y] ? qRColor.Foreground(x, y, "").ToAndroid().ToArgb() : qRColor.Background(x, y).ToAndroid().ToArgb();
+                    outputIndex++;
+                }
+            }
+
+            var bitmap = Bitmap.CreateBitmap(width, height, Bitmap.Config.Argb8888);
+            bitmap.SetPixels(pixels, 0, width, 0, 0, width, height);
+            return bitmap;
+        }
+    }
+}
---
+++ XF_FirebaseProjectClient/XF_FirebaseProjectClient/XF_FirebaseProjectClient.Android/ExtendedZXingBarcodeImageViewRenderer.cs
@@ -0,0 +1,68 @@
+﻿
+using System.ComponentModel;
+using Android.Content;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XF_FirebaseProjectClient.Droid;
+using XF_FirebaseProjectClient.Renderers;
+
+[assembly: ExportRenderer(typeof(ExtendedZXingBarcodeImageView), typeof(ExtendedZXingBarcodeImageViewRenderer))]
+namespace XF_FirebaseProjectClient.Droid
+{
+    public class ExtendedZXingBarcodeImageViewRenderer : ViewRenderer<ExtendedZXingBarcodeImageView, ImageView>
+    {
+        ExtendedZXingBarcodeImageView formsView;
+        ImageView imageView;
+
+        public ExtendedZXingBarcodeImageViewRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Regenerate();
+
+            base.OnElementPropertyChanged(sender, e);
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<ExtendedZXingBarcodeImageView> e)
+        {
+            formsView = Element;
+
+            if (imageView == null)
+            {
+                imageView = new ImageView(Context);
+                base.SetNativeControl(imageView);
+            }
+
+            Regenerate();
+
+            base.OnElementChanged(e);
+        }
+
+        void Regenerate()
+        {
+            if (!string.IsNullOrEmpty(formsView?.BarcodeValue))
+            {
+                var writer = new ZXing.Mobile.BarcodeWriter
+                {
+                    Renderer = new BitmapRenderer(formsView.QRColor)
+                };
+
+                if (formsView != null && formsView.BarcodeOptions != null)
+                    writer.Options = formsView.BarcodeOptions;
+
+                writer.Format = formsView.BarcodeFormat;
+
+                var code = formsView.BarcodeValue;
+
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    var image = writer.Write(code);
+                    imageView.SetImageBitmap(image);
+                });
+            }
+        }
+    }
+}
